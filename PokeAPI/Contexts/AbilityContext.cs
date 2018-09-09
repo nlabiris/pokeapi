0484d2d13de0a3a508a97317572d4eb1cdcea9fe
Nikos Labiris <nick.f1@hotmail.com>
2018-09-09 21:59:42 +0300

    Refactor SQL queries as issued in #3 and initialize view model classes

--- PokeAPI/Contexts/AbilityContext.cs
+++ PokeAPI/Contexts/AbilityContext.cs
@@ -4,176 +4,59 @@ using System.Data;
 using System.Linq;
 using System.Web;
 using PokeAPI.Models;
+using PokeAPI.ViewModels;
 using PokeAPI.Helper;
 using PokeAPI.DataAccess;
 
 namespace PokeAPI.Contexts {
     public class AbilityContext : DataWorker {
+        private readonly AbilityViewModel VM_Ability = null;
+        private readonly GenerationViewModel VM_Generation = null;
+        private readonly RegionViewModel VM_Region = null;
+        private readonly VersionGroupsViewModel VM_VersionGroups = null;
+        private readonly LanguageViewModel VM_Language = null;
+
+        public AbilityContext() {
+            if (VM_Ability == null) {
+                VM_Ability = new AbilityViewModel();
+            }
+            if (VM_Generation == null) {
+                VM_Generation = new GenerationViewModel();
+            }
+            if (VM_Region == null) {
+                VM_Region = new RegionViewModel();
+            }
+            if (VM_VersionGroups == null) {
+                VM_VersionGroups = new VersionGroupsViewModel();
+            }
+            if (VM_Language == null) {
+                VM_Language = new LanguageViewModel();
+            }
+        }
+
         public IEnumerable<Ability> AllAbilities() {
-            List<Ability> abilities = new List<Ability>();
+            List<Ability> abilities = null;
             using (IDbConnection connection = database.CreateOpenConnection()) {
-                using (IDbCommand command = database.CreateCommand()) {
-                    command.Connection = connection;
-                    command.CommandText = Query.GetAllAbilities;
-                    command.Prepare();
-                    using (IDataReader reader = command.ExecuteReader()) {
-                        while (reader.Read()) {
-                            Ability ability = new Ability {
-                                Id = reader.CheckValue<int>("id"),
-                                Identifier = reader.CheckObject<string>("identifier"),
-                                Generation = new Generation {
-                                    Id = reader.CheckValue<int>("generation_id")
-                                },
-                                IsMainSeries = reader.CheckValue<bool>("is_main_series")
-                            };
-                            abilities.Add(ability);
-                        }
-                    }
-                } // Command
+                abilities = VM_Ability.RetrieveAllAbilities(connection);
 
                 foreach (Ability a in abilities) {
-                    using (IDbCommand command = database.CreateCommand()) {
-                        command.Connection = connection;
-                        command.CommandText = Query.GetSpecificGeneration;
-                        command.Prepare();
-                        command.AddWithValue("@generations_id", a.Generation.Id);
-                        using (IDataReader reader = command.ExecuteReader()) {
-                            if (reader.Read()) {
-                                a.Generation = new Generation {
-                                    Id = reader.CheckValue<int>("id"),
-                                    MainRegion = new Region {
-                                        Id = reader.CheckValue<int>("main_region_id")
-                                    },
-                                    Identifier = reader.CheckObject<string>("identifier")
-                                };
-                            }
-                        }
-                    } // Command
-
-                    if (a.Generation.MainRegion != null) {
-                        using (IDbCommand command = database.CreateCommand()) {
-                            command.Connection = connection;
-                            command.CommandText = Query.GetSpecificRegion;
-                            command.Prepare();
-                            command.AddWithValue("@regions_id", a.Generation.MainRegion.Id);
-                            using (IDataReader reader = command.ExecuteReader()) {
-                                if (reader.Read()) {
-                                    a.Generation.MainRegion = new Region {
-                                        Id = reader.CheckValue<int>("id"),
-                                        Identifier = reader.CheckObject<string>("identifier")
-                                    };
-                                }
-                            }
-                        } // Command
+                    a.Generation = VM_Generation.RetrieveSpecificGeneration(connection, a.Generation.Id);
+                    
+                    if (a.Generation != null && a.Generation.MainRegion != null) {
+                        a.Generation.MainRegion = VM_Region.RetrieveSpecificRegion(connection, a.Generation.MainRegion.Id);
                     }
 
-                    using (IDbCommand command = database.CreateCommand()) {
-                        command.Connection = connection;
-                        command.CommandText = Query.GetSpecificAbilityChangelog;
-                        command.Prepare();
-                        command.AddWithValue("@ability_id", a.Id);
-                        using (IDataReader reader = command.ExecuteReader()) {
-                            if (reader.Read()) {
-                                a.AbilityChangelog = new AbilityChangelog {
-                                    Id = reader.CheckValue<int>("id"),
-                                    ChangedInVersionGroup = new VersionGroups {
-                                        Id = reader.CheckValue<int>("changed_in_version_group_id")
-                                    }
-                                };
-                            }
-                        }
-                    } // Command
-
+                    a.AbilityChangelog = VM_Ability.RetrieveSpecificAbilityChangelog(connection, a.Id);
                     if (a.AbilityChangelog != null && a.AbilityChangelog.ChangedInVersionGroup != null) {
-                        using (IDbCommand command = database.CreateCommand()) {
-                            command.Connection = connection;
-                            command.CommandText = Query.GetSpecificVersionGroup;
-                            command.Prepare();
-                            command.AddWithValue("@version_group_id", a.AbilityChangelog.ChangedInVersionGroup.Id);
-                            using (IDataReader reader = command.ExecuteReader()) {
-                                if (reader.Read()) {
-                                    a.AbilityChangelog.ChangedInVersionGroup = new VersionGroups {
-                                        Id = reader.CheckValue<int>("id"),
-                                        Identifier = reader.CheckObject<string>("identifier"),
-                                        Generation = new Generation {
-                                            Id = reader.CheckValue<int>("generation_id")
-                                        },
-                                        Order = reader.CheckValue<int>("order")
-                                    };
-                                }
-                            }
-                        } // Command
-
-                        using (IDbCommand command = database.CreateCommand()) {
-                            command.Connection = connection;
-                            command.CommandText = Query.GetSpecificAbilityChangelogProse;
-                            command.Prepare();
-                            command.AddWithValue("@ability_changelog_id", a.AbilityChangelog.Id);
-                            using (IDataReader reader = command.ExecuteReader()) {
-                                if (reader.Read()) {
-                                    a.AbilityChangelog.AbilityChangelogProse = new AbilityChangelogProse {
-                                        LocalLanguage = new Language {
-                                            Id = reader.CheckValue<int>("local_language_id")
-                                        },
-                                        Effect = reader.CheckObject<string>("effect")
-                                    };
-                                }
-                            }
-                        } // Command
-
-                        using (IDbCommand command = database.CreateCommand()) {
-                            command.Connection = connection;
-                            command.CommandText = Query.GetSpecificLanguage;
-                            command.Prepare();
-                            command.AddWithValue("@language_id", a.AbilityChangelog.AbilityChangelogProse.LocalLanguage.Id);
-                            using (IDataReader reader = command.ExecuteReader()) {
-                                if (reader.Read()) {
-                                    a.AbilityChangelog.AbilityChangelogProse.LocalLanguage = new Language {
-                                        Id = reader.CheckValue<int>("id"),
-                                        Iso639 = reader.CheckObject<string>("iso639"),
-                                        Iso3166 = reader.CheckObject<string>("iso3166"),
-                                        Identifier = reader.CheckObject<string>("identifier"),
-                                        OfficialIndex = reader.CheckValue<bool>("official"),
-                                        Order = reader.CheckValue<int>("order")
-                                    };
-                                }
-                            }
-                        } // Command
+                        a.AbilityChangelog.ChangedInVersionGroup = VM_VersionGroups.RetrieveSpecificVersionGroups(connection, a.AbilityChangelog.ChangedInVersionGroup.Id);
+                        a.AbilityChangelog.AbilityChangelogProse = VM_Ability.RetrieveSpecificAbilityChangelogProse(connection, a.AbilityChangelog.Id);
+                        a.AbilityChangelog.AbilityChangelogProse.LocalLanguage = VM_Language.RetrieveSpecificLanguage(connection, a.AbilityChangelog.AbilityChangelogProse.LocalLanguage.Id);
 
                         if (a.AbilityChangelog.ChangedInVersionGroup.Generation != null) {
-                            using (IDbCommand command = database.CreateCommand()) {
-                                command.Connection = connection;
-                                command.CommandText = Query.GetSpecificGeneration;
-                                command.Prepare();
-                                command.AddWithValue("@generations_id", a.AbilityChangelog.ChangedInVersionGroup.Generation.Id);
-                                using (IDataReader reader = command.ExecuteReader()) {
-                                    if (reader.Read()) {
-                                        a.AbilityChangelog.ChangedInVersionGroup.Generation = new Generation {
-                                            Id = reader.CheckValue<int>("id"),
-                                            MainRegion = new Region {
-                                                Id = reader.CheckValue<int>("main_region_id")
-                                            },
-                                            Identifier = reader.CheckObject<string>("identifier")
-                                        };
-                                    }
-                                }
-                            } // Command
+                            a.AbilityChangelog.ChangedInVersionGroup.Generation = VM_Generation.RetrieveSpecificGeneration(connection, a.AbilityChangelog.ChangedInVersionGroup.Generation.Id);
 
                             if (a.AbilityChangelog.ChangedInVersionGroup.Generation.MainRegion != null) {
-                                using (IDbCommand command = database.CreateCommand()) {
-                                    command.Connection = connection;
-                                    command.CommandText = Query.GetSpecificRegion;
-                                    command.Prepare();
-                                    command.AddWithValue("@regions_id", a.AbilityChangelog.ChangedInVersionGroup.Generation.MainRegion.Id);
-                                    using (IDataReader reader = command.ExecuteReader()) {
-                                        if (reader.Read()) {
-                                            a.AbilityChangelog.ChangedInVersionGroup.Generation.MainRegion = new Region {
-                                                Id = reader.CheckValue<int>("id"),
-                                                Identifier = reader.CheckObject<string>("identifier")
-                                            };
-                                        }
-                                    }
-                                } // Command
+                                a.AbilityChangelog.ChangedInVersionGroup.Generation.MainRegion = VM_Region.RetrieveSpecificRegion(connection, a.AbilityChangelog.ChangedInVersionGroup.Generation.MainRegion.Id);
                             }
                         }
                     }
@@ -247,5 +130,5 @@ namespace PokeAPI.Contexts {
             } // Connection
             return ability;
         }
-    }
-}
+    } //class
+} // namespace
